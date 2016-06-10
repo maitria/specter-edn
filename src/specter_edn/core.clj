53a56f5a4587cb80ac6482dd6312a3c0f1686bf6
Jason Felice <jason.m.felice@gmail.com>
2016-06-10 15:47:34 -0400

    (select [SEXPR] ...) works

--- src/specter_edn/core.clj
+++ src/specter_edn/core.clj
@@ -1,6 +1,14 @@
-(ns specter-edn.core)
+(ns specter-edn.core
+  (:require [clojure.edn :as edn]
+            [clojure.java.io :as io]
+            [com.rpl.specter.protocols]
+            [rewrite-clj.parser :as p]))
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+(deftype SEXPR-TYPE [])
+
+(extend-protocol com.rpl.specter.protocols/Navigator
+  SEXPR-TYPE
+  (select* [_ source-code next-fn]
+    (next-fn (edn/read-string source-code))))
+
+(def SEXPR (->SEXPR-TYPE))
--- test/specter_edn/core_test.clj
+++ test/specter_edn/core_test.clj
@@ -1,7 +1,13 @@
 (ns specter-edn.core-test
   (:require [clojure.test :refer :all]
+            [com.rpl.specter :refer :all]
+            [com.rpl.specter.macros :refer :all]
             [specter-edn.core :refer :all]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+(deftest SEXPR-test
+  (testing "can select inside stringified s-expressions"
+    (are [source-code path result] (= (select path source-code) result)
+      "[]"         [SEXPR]       [[]]
+      "[1 2]"      [SEXPR FIRST] [1]
+      ["3"]        [FIRST SEXPR] [3]
+      "[1 ;??\n2]" [SEXPR LAST]  [2])))
