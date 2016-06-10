519316a1ac554ed4639aa18c69ed239db9fd0c98
Jason Felice <jason.m.felice@gmail.com>
2016-06-10 14:29:26 -0400

    Initial commit

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to specter-edn
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/specter_edn/core_test.clj
@@ -0,0 +1,7 @@
+(ns specter-edn.core-test
+  (:require [clojure.test :refer :all]
+            [specter-edn.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
