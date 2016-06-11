4d5b43e63b013ed3ee6ce2c6fde9946643a0e6b0
Jason Felice <jason.m.felice@gmail.com>
2016-06-11 13:27:52 -0400

    Transform replaces values in the string

--- src/specter_edn/core.clj
+++ src/specter_edn/core.clj
@@ -9,6 +9,9 @@
 (extend-protocol com.rpl.specter.protocols/Navigator
   SEXPR-TYPE
   (select* [_ source-code next-fn]
-    (next-fn (edn/read-string source-code))))
+    (next-fn (edn/read-string source-code)))
+  (transform* [_ source-code next-fn]
+    (let [s-expr (next-fn (edn/read-string source-code))]
+      (pr-str s-expr))))
 
 (def SEXPR (->SEXPR-TYPE))
--- test/specter_edn/core_test.clj
+++ test/specter_edn/core_test.clj
@@ -6,8 +6,12 @@
 
 (deftest SEXPR-test
   (testing "can select inside stringified s-expressions"
-    (are [source-code path result] (= (select path source-code) result)
+    (are [structure path result] (= (select path structure) result)
       "[]"         [SEXPR]       [[]]
       "[1 2]"      [SEXPR FIRST] [1]
       ["3"]        [FIRST SEXPR] [3]
-      "[1 ;??\n2]" [SEXPR LAST]  [2])))
+      "[1 ;??\n2]" [SEXPR LAST]  [2]))
+
+  (testing "can transform inside stringified s-expressions"
+    (are [structure path replacement result] (= (setval path replacement structure) result)
+      "[1]"        [SEXPR FIRST] 2 "[2]")))
