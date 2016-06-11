f7fb32147eddfe66e712e5ae958b6148b610b8eb
Jason Felice <jason.m.felice@gmail.com>
2016-06-11 18:38:06 -0400

    Version 0.1.0

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject com.maitria/specter-edn "0.1.0-SNAPSHOT"
+(defproject com.maitria/specter-edn "0.1.0"
   :description "Specter paths for working with formatted EDN and Clojure code"
   :url "http://github.com/eraserhd/specter-edn"
   :license {:name "Eclipse Public License"
