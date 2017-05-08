1e3435d879cf8f5904218b5fb0f3b2f3b6835eb7
Jason Felice <jason.m.felice@gmail.com>
2017-05-08 10:38:57 -0400

    Configure credentials

--- project.clj
+++ project.clj
@@ -3,7 +3,10 @@
   :url "http://github.com/eraserhd/specter-edn"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :deploy-repositories [["releases" :clojars]]
+  :deploy-repositories [["releases"
+                         {:url "https://clojars.org/repo/"
+                          :username :env/clojars_username
+                          :password :env/clojars_password}]]
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [org.clojure/core.match "0.3.0-alpha4"]
                  [com.rpl/specter "1.0.1"]
